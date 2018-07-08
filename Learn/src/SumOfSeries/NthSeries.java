96a2cadddc088bfa3e3066824e08480bcc971570
Sergii Senatorov <sergii.senatorov@gmail.com>
2018-07-08 17:21:00 +0300

    08.07.2018

---
+++ Learn/src/SumOfSeries/NthSeries.java
@@ -0,0 +1,20 @@
+package SumOfSeries;
+
+import java.util.Locale;
+
+public class NthSeries
+{
+    public static String seriesSum(int n)
+    {
+        // Happy Coding ^_^
+        int znamenatel = 1;
+        float res = 0f;
+        for(int i=0 ;i < n ;i++)
+        {
+            // знаменатель = 1 + n*i;
+            res = res + (1 / (1 + 3f * i));
+        }
+        return String.format(Locale.ENGLISH,"%.2f",res);
+        // return Float.toString(res).;
+    }
+}
