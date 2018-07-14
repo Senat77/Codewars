8d9c4bcf61c081cfefcaa717298282eb40dce8a5
Sergii Senatorov <sergii.senatorov@gmail.com>
2018-07-14 22:29:47 +0300

    ValidBraces

---
+++ Learn/src/ValidBraces/BraceChecker.java
@@ -0,0 +1,35 @@
+package ValidBraces;
+
+public class BraceChecker
+{
+    public static  boolean isValid(String braces)
+    {
+        // Add code here
+        System.out.println("Braces set = " + braces);
+        String[] braceTempl = {"{}","()","[]"};
+        boolean flag = false;
+
+        if(braces.length() % 2 != 0) return false;
+
+        while(true)
+        {
+            flag = false; // ни один шаблон не найден
+
+            for(int i=0;i<3;i++)
+            {
+                if (braces.indexOf(braceTempl[i]) != -1)
+                {
+                    braces = braces.substring(0, braces.indexOf(braceTempl[i])) +
+                            braces.substring(braces.indexOf(braceTempl[i]) + 2, braces.length());
+                    System.out.println(braces);
+                    flag = true;
+                    break;
+                }
+            }
+
+            if(!flag && !braces.isEmpty()) return false;
+
+            if(braces.isEmpty()) return true;
+        }
+    }
+}
