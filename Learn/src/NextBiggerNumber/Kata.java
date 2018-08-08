2d03d3aa1cae03a330848eb3b41ccd70186d96a0
Sergii Senatorov <sergii.senatorov@gmail.com>
2018-08-08 17:21:18 +0300

    NextBiggerNumber started

---
+++ Learn/src/NextBiggerNumber/Kata.java
@@ -0,0 +1,38 @@
+package NextBiggerNumber;
+
+/*
+http://www.codewars.com/kata/55983863da40caa2c900004e/train/java
+
+You have to create a function that takes a positive integer number and returns the next bigger number formed by the
+same digits:
+
+Kata.nextBiggerNumber(12)==21
+Kata.nextBiggerNumber(513)==531
+Kata.nextBiggerNumber(2017)==2071
+If no bigger number can be composed using those digits, return -1:
+
+Kata.nextBiggerNumber(9)==-1
+Kata.nextBiggerNumber(111)==-1
+Kata.nextBiggerNumber(531)==-1
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Kata
+{
+    public static void main(String[] args)
+    {
+        //System.out.println(nextBiggerNumber(12));
+        System.out.println(nextBiggerNumber(513));
+        //System.out.println(nextBiggerNumber(2017));
+        //System.out.println(nextBiggerNumber(9));
+        //System.out.println(nextBiggerNumber(111));
+        //System.out.println(nextBiggerNumber(531));
+    }
+
+    public static long nextBiggerNumber(long n)
+    {
+
+    }
+}
