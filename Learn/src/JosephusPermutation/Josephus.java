97047d5623b81de5376358687416feb430739226
Sergii Senatorov <sergii.senatorov@gmail.com>
2018-08-14 17:26:17 +0300

    JosephusPermutation started

---
+++ Learn/src/JosephusPermutation/Josephus.java
@@ -0,0 +1,18 @@
+package JosephusPermutation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Josephus
+{
+    public static <T> List<T> josephusPermutation(final List<T> items, final int k)
+    {
+        ArrayList <T> l = new ArrayList<>(items);
+        ArrayList <T> res = new ArrayList<>();
+
+        while(l.isEmpty())
+        {
+            
+        }
+    }
+}
